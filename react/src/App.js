6728b03d119646e825e8eaba1d4a31ad4d68cdec
wiktorsztefko <wiktorsztefko@macbookpro.home>
2025-11-11 14:30:03 +0100

    feat(frontend): add upload page

--- react/src/App.js
+++ react/src/App.js
@@ -8,12 +8,14 @@ import NotFound from "./components/NotFound";
 import GalleryAlcohols from './components/GalleryAlcohols';
 import GalleryCocktails from './components/GalleryCocktails';
 import Cocktail from './components/Cocktail';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Upload />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -21,6 +23,7 @@ function App() {
         <Route path="/alcohols" element={<GalleryAlcohols/>} />
         <Route path="/cocktails" element={<GalleryCocktails />} />
         <Route path="/cocktails/:slug" element={<Cocktail />} />
+        <Route path="/upload" element={<Upload />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- react/src/components/Upload.js
+++ react/src/components/Upload.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import { fetchUser } from "../api/fetchUser";
 import { API_URL } from "../api/url";
@@ -18,6 +18,113 @@ import Footer from "./Footer";
 const Upload= () => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
+
+    const [ingredientsList, setIngredientsList] = useState([]);             // lista dostępnych składników z API
+    const [unitsList, setUnitsList] = useState([]);                         // lista jednostek z API
+    const [selectedIngredients, setSelectedIngredients] = useState([]);     // aktualnie wybrane składniki w formularzu
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [preparationInstruction, setPreparationInstruction] = useState("");
+    const [funFact, setFunFact] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [file, setFile] = useState(null);
+
+    const [errors, setErrors] = useState({
+        name: "",
+        description: "",
+        preparationInstruction: "",
+        difficultyLevel: "",
+        ingredients: [], // tablica błędów dla każdego składnika
+        file: "",
+    });
+
+    useEffect(() => {
+        // Pobranie składników
+        fetch(`${API_URL}/api/reference/ingredients`, { credentials: 'include' })
+            .then(res => res.json())
+            .then(data => setIngredientsList(data))
+            .catch(err => console.error(err));
+
+        // Pobranie jednostek
+        fetch(`${API_URL}/api/reference/units`, { credentials: 'include' })
+            .then(res => res.json())
+            .then(data => setUnitsList(data))
+            .catch(err => console.error(err));
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!validateForm()) return;
+
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("description", description);
+        formData.append("preparationInstruction", preparationInstruction);
+        formData.append("funFact", funFact);
+        formData.append("difficultyLevel", difficulty);
+        formData.append("file", file);
+
+        selectedIngredients.forEach((ing, idx) => {
+            formData.append(`ingredients[${idx}][name]`, ing.name);
+            formData.append(`ingredients[${idx}][quantity]`, ing.quantity);
+            formData.append(`ingredients[${idx}][unit]`, ing.unit);
+        });
+
+        // const response = await fetch(`${API_URL}/api/cocktails`, {
+        //     method: "POST",
+        //     body: formData,
+        //     credentials: "include",
+        // });
+
+        // if (response.ok) {
+        //     // np. przekierowanie do listy koktajli
+        // } else {
+        //     // obsługa błędów
+        // }
+    };
+
+    const validateForm = () => {
+        const newErrors = {
+            name: name.trim() ? "" : "Podaj nazwę koktajlu",
+            description: description.trim() ? "" : "Podaj opis",
+            preparationInstruction: preparationInstruction.trim() ? "" : "Podaj instrukcję przygotowania",
+            difficultyLevel: difficulty ? "" : "Wybierz poziom trudności",
+            ingredients: [],
+            ingredientsGlobal: "",
+            file: file ? "" : "Wybierz plik z obrazkiem",
+        };
+
+        if (selectedIngredients.length === 0) {
+            newErrors.ingredientsGlobal = "Dodaj conajmniej 1 składnik";
+        }
+     
+        selectedIngredients.forEach((ing, idx) => {
+            console.log(ing)
+            const ingErrors = {
+                name: ing.id_ingredient ? "" : "Wybierz składnik",
+                quantity: ing.quantity ? "" : "Wybierz ilość",
+                unit: ing.id_unit ? "" : "Wybierz jednostkę",
+            };
+            newErrors.ingredients[idx] = ingErrors;
+        });
+
+        setErrors(newErrors);
+
+        // Jeśli wszystkie wartości w newErrors są puste → formularz jest poprawny
+        const isValid =
+            !newErrors.name &&
+            !newErrors.description &&
+            !newErrors.preparationInstruction &&
+            !newErrors.funFact &&
+            !newErrors.difficultyLevel &&
+            !newErrors.file &&
+            !newErrors.ingredientsGlobal &&
+            newErrors.ingredients.every((ingErr) => Object.keys(ingErr).length === 0);
+
+        return isValid;
+    };
 
     useEffect(() => {
         const getUser = async () => {
@@ -43,12 +150,222 @@ const Upload= () => {
         <>
             <Header />
             <div className="columns is-gapless">
+
                 <div className="column is-2">
                     <Navigation />
                 </div>
+
                 <main className="column panel-upload">
 
+                    <form onSubmit={handleSubmit} >
+                        <h1 className="title">Dodaj koktajl</h1>
 
+                        {/* Nazwa koktajlu*/}
+                        <div className="field">
+                            <label className="label">Nazwa koktajlu</label>
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    placeholder="Wpisz nazwę koktajlu"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                            {errors.name && (
+                                <p className="has-text-danger">{errors.name}</p>
+                            )}
+                        </div>
+
+                        {/* Opis koktajlu*/}
+                        <div className="field">
+                            <label className="label">Opis koktajlu</label>
+                            <div className="control">
+                                <textarea
+                                    className="textarea"
+                                    placeholder="Wpisz opis koktajlu"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                />
+                            </div>
+                            {errors.description && (
+                                <p className="has-text-danger">{errors.description}</p>
+                            )}
+                        </div>
+
+                        {/* Instrukcja przygotowania*/}
+                        <div className="field">
+                            <label className="label">Instrukcja przygotowania</label>
+                            <div className="control">
+                                <textarea
+                                    className="textarea"
+                                    placeholder="Wpisz instrukcję przygotowania"
+                                    value={preparationInstruction}
+                                    onChange={(e) => setPreparationInstruction(e.target.value)}
+                                />
+                            </div>
+                            {errors.preparationInstruction && (
+                                <p className="has-text-danger">{errors.preparationInstruction}</p>
+                            )}
+                        </div>
+
+                        {/* Ciekawostka o koktajlu */}
+                        <div className="field">
+                            <label className="label">Ciekawostka o koktajlu (opcjonalnie)</label>
+                            <div className="control">
+                                <textarea
+                                    className="textarea"
+                                    placeholder="Wpisz ciekawostkę"
+                                    value={funFact}
+                                    onChange={(e) => setFunFact(e.target.value)}
+                                />
+                            </div>
+                        </div>
+
+                        {/* Poziom trudności */}
+                        <div className="field">
+                            <label className="label mt-3">Poziom trudności</label>
+                            <select
+                                value={difficulty}
+                                onChange={(e) => setDifficulty(e.target.value)}
+                                className="input"
+                            >
+                                
+                                <option value="" disabled hidden>Wybierz poziom trudności</option>
+                                {[1, 2, 3, 4, 5].map(n => (
+                                    <option key={n} value={n}>{n}</option>
+                                ))}
+                            </select>
+
+                            {errors.difficultyLevel && (
+                                <p className="has-text-danger">{errors.difficultyLevel}</p>
+                            )}
+                        </div>
+
+                        {/* Składniki koktajlu */}
+                        <div id="ingredients-div" className="flex-column mt-3">
+
+                            <label className="label">Składniki</label>
+                            {errors.ingredientsGlobal && (
+                                <p className="has-text-danger">{errors.ingredientsGlobal}</p>
+                            )}
+
+                            {selectedIngredients.map((ingredient, index) => (
+                                <div>
+                                    <div key={index} className="ingredient-container is-flex is-align-items-center gap-2 mb-3">
+                                        
+                                        <select
+                                            className="ingredients-select input"
+                                            value={ingredient.id_ingredient || ""}
+                                            onChange={(e) => {
+                                                const updated = [...selectedIngredients];
+                                                updated[index].id_ingredient = e.target.value;
+                                                setSelectedIngredients(updated);
+                                            }}
+                                        >
+                                            <option value="" disabled hidden>Wybierz składnik</option>
+                                            {ingredientsList.map(i => (
+                                                <option key={i.id_ingredient} value={i.id_ingredient}>{i.name}</option>
+                                            ))}
+                                        </select>
+
+                                        <input
+                                            type="number"
+                                            min="1"
+                                            max="200"
+                                            className="count-select input"
+                                            value={ingredient.quantity || ""}
+                                            onChange={(e) => {
+                                                const updated = [...selectedIngredients];
+                                                updated[index].quantity = e.target.value;
+                                                setSelectedIngredients(updated);
+                                            }}
+                                            placeholder="Ilość"
+                                        />
+
+                                        <select
+                                            className="unit-select input"
+                                            value={ingredient.id_unit || ""}
+                                            onChange={(e) => {
+                                                const updated = [...selectedIngredients];
+                                                updated[index].id_unit = e.target.value;
+                                                setSelectedIngredients(updated);
+                                            }}
+                                        >
+                                            <option value="" disabled hidden>Wybierz jednostkę</option>
+                                            {unitsList.map(u => (
+                                                <option key={u.id_unit} value={u.id_unit}>{u.name}</option>
+                                            ))}
+                                        </select>
+
+                                        
+                                        <button
+                                            type="button"
+                                                className="delete-button flex-center button is-warning"
+                                                onClick={() => {
+                                                    const updated = [...selectedIngredients];
+                                                    updated.splice(index, 1);
+                                                    setSelectedIngredients(updated);
+                                                }}
+                                        >
+                                        <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                      
+                                    </div>
+
+                                    <div className="mb-3">
+                                        {errors.ingredients[index]?.name && (
+                                            <p className="has-text-danger">{errors.ingredients[index].name}</p>
+                                        )}
+                                        {errors.ingredients[index]?.quantity && (
+                                            <p className="has-text-danger">{errors.ingredients[index].quantity}</p>
+                                        )}
+                                        {errors.ingredients[index]?.unit && (
+                                            <p className="has-text-danger">{errors.ingredients[index].unit}</p>
+                                        )}
+                                    </div>
+                                </div>
+                            ))}
+
+                            <button type="button" className="button is-warning"
+                                onClick={() => setSelectedIngredients([...selectedIngredients, {}])}
+                            >
+                                Dodaj składnik
+                            </button>
+
+                        </div>
+
+                        {/* Grafika koktajlu */}
+                        <div className="field">
+                            <label className="label mt-3">Grafika koktajlu</label>
+                            <input
+                                className="input"
+                                type="file"
+                                id="file-upload"
+                                style={{ display: "none" }}
+                                accept="image/*"
+                                onChange={(e) => setFile(e.target.files[0])}
+                            />
+                            <div className="file-div">
+                                <label
+                                    htmlFor="file-upload"
+                                    className="button is-warning label-file"
+                                >
+                                    Wybierz plik
+                                </label>
+                                <p className="ml-2">{file ? file.name : "Nie wybrano pliku"}</p>
+                            </div>
+                            {errors.file && (
+                                <p className="has-text-danger">{errors.file}</p>
+                            )}
+                        </div>
+
+                        {/* Button submit */}
+                        <div className="submit-div mt-3">
+                            <button type="submit" className="button is-warning mt-1">Wyślij</button>
+                        </div>
+
+                    </form>
                 </main>
             </div>
             <Footer />
